2513cd7fde6a62aeeca32af211abd0e8eb82d63d
VitalieVS <54939321+VitalieVS@users.noreply.github.com>
2019-12-29 16:53:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -20,20 +20,20 @@ function clearField(div, description, startG) {
 
 function timerStart(){
     let time = 0;
-    document.write('<div id="timer">'+time+'</div>');
     let startTimer = setInterval(function () {
         time += 1;
         textModify(time);
-        if (time >= 3){
+        if (time > 3){
             clearInterval(startTimer);
             timerControl();
+            startText();
         }
     }, 1000)
 }
 
 function textModify(time){
     let timer = document.getElementById("timer");
-    timer.innerHTML = time;
+    timer.innerHTML = "Start in... " + time + " seconds";
 }
 
 function timerControl() {
@@ -47,4 +47,40 @@ function getTimerData() {
 
 function deleteTimer(timer){
     timer.parentNode.removeChild(timer);
+}
+
+function startText() {
+    createText();
+    moveText();
+}
+
+function createText() {
+    let text = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    addNewDiv(text)
+}
+
+function addNewDiv(text){
+    let textDiv = document.createElement("div");
+    textDiv.className = 'text-game';
+    textDiv.innerHTML = text;
+    document.getElementById('contain').appendChild(textDiv);
+}
+
+function moveText(){
+    let text = document.getElementsByClassName("text-game");
+    let time = setInterval(frame, 10); //this one will come random
+    let pos = 0;
+    function frame(){
+        if (pos === 600){
+            destroyText(text[0]);
+            clearInterval(time);
+        } else{
+            pos++;
+            text[0].style.top = pos + 'px';
+        }
+    }
+}
+
+function destroyText(text){
+    text.parentNode.removeChild(text);
 }
